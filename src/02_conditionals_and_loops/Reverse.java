cd8792874ac9dd5acb5d8500d1214f309cc77cd3
DevMaverickMB <milindbhushan007@gmail.com>
2026-03-31 20:15:37 +0530

    day 2

---
+++ src/02_conditionals_and_loops/Reverse.java
@@ -0,0 +1,16 @@
+public class Reverse {
+    public static void main(String[] args) {
+        int n = 23597321;
+        int rev_num = 0;
+
+        while(n > 0){
+            int rem = n % 10;
+
+            rev_num = (rev_num*10) + rem;
+
+            n = n / 10;
+        }
+
+        System.out.println(rev_num);
+    }
+}
